2ec882c4a2a4137c2bfcb4c02a98b853cfb59fe1
Mostafa <mostafa56569855@gmail.com>
2023-06-05 12:26:39 +0200

    init commit

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@ import router from "./router";
 import store from "./store";
 import "./scss/main.scss";
 import "normalize.css";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
